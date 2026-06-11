47b215e012a2a4fb298dc21a9b4bdcc78be333f7
dwn <officiald4wn@gmail.com>
2026-06-11 19:28:06 +0800

    nav

---
+++ shared_pages.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    $("#about").click(() => {
+        window.open("../about/about.html", "_self", "noopener,noreferrer");
+    });
+
+    $("#contacts").click(() => {
+        window.open("../contacts/contacts.html", "_self", "noopener,noreferrer");
+    });
+
+    $("#projects").click(() => {
+        window.open("../projects/projects.html", "_self", "noopener,noreferrer");
+    });
+
+    $("#something").click(() => {
+        window.open("../something/something.html", "_self", "noopener,noreferrer");
+    });
+})
